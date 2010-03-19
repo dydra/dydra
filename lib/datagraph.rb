83588ef1d6bfb48feecb8be3fc6e0f56af665a42
Arto Bendiken <arto@bendiken.net>
2010-03-19 03:09:10 +0100

    Defined the base URL in terms of the DATAGRAPH_URL environment variable.

--- lib/datagraph.rb
+++ lib/datagraph.rb
@@ -3,7 +3,7 @@ require 'datagraph/version'
 
 ##
 module Datagraph
-  URL = RDF::URI.new('http://datagraph.org/')
+  URL = RDF::URI.new(ENV['DATAGRAPH_URL'] || 'http://datagraph.org/')
 
   autoload :Client, 'datagraph/client'
 end
