6094c0679e8d30dfe732aab3b2c9e92ae14852c6
Arto Bendiken <arto@bendiken.net>
2010-03-19 03:53:11 +0100

    Implemented Client#get.

--- lib/datagraph/client/resource.rb
+++ lib/datagraph/client/resource.rb
@@ -1,7 +1,11 @@
+require 'net/http'
+
 module Datagraph::Client
   ##
   # Base class for Datagraph.org resources.
   class Resource
+    HEADERS = {'Accept' => 'text/plain'} # N-Triples
+
     ##
     # @return [RDF::URI]
     attr_reader :url
@@ -18,6 +22,24 @@ module Datagraph::Client
     # @return [RDF::URI]
     def to_uri
       url
+    end
+
+    ##
+    # Performs an HTTP GET request on this resource.
+    #
+    # @param  [Hash{String => String}] headers
+    # @yield  [response]
+    # @yieldparam [Net::HTTPResponse] response
+    # @return [Net::HTTPResponse]
+    def get(headers = {}, &block)
+      Net::HTTP.start(url.host, url.port) do |http|
+        response = http.get(url.path.to_s, HEADERS.merge(headers))
+        if block_given?
+          block.call(response)
+        else
+          response
+        end
+      end
     end
   end # class Resource
 end # module Datagraph::Client
