d523c1c7ab458275a77e0ba1ac58ca4188520a5b
Arto Bendiken <arto@bendiken.net>
2010-03-19 04:58:29 +0100

    Implemented Query#initialize.

--- lib/datagraph/client/query.rb
+++ lib/datagraph/client/query.rb
@@ -1,6 +1,34 @@
 module Datagraph::Client
   ##
+  # Represents a Datagraph.org SPARQL query.
   class Query < Resource
-    # TODO
+    SPEC = %r(^([^/\.]+)/([^/\.]+)/([^/\.]+)$)
+
+    attr_reader :repository
+    attr_reader :name
+
+    ##
+    # @param  [String, #to_s] repository_name
+    # @param  [String, #to_s] name
+    def initialize(repository_name, name)
+      @repository = case repository_name
+        when Repository then repository_name
+        else Repository.new(*repository_name.to_s.split('/'))
+      end
+      @name = name.to_s
+      super(Datagraph::URL.join(path))
+    end
+
+    ##
+    # Returns the path spec of this query.
+    def path
+      [repository.to_s, name].join('/')
+    end
+
+    ##
+    # Returns a string representation of the query name.
+    def to_s
+      path
+    end
   end # class Query
 end # module Datagraph::Client
