f395129542c07ecba5fa8debc8e45ec1df6ad1d0
Arto Bendiken <arto@bendiken.net>
2010-03-19 03:31:20 +0100

    Implemented Account#initialize.

--- lib/datagraph/client/account.rb
+++ lib/datagraph/client/account.rb
@@ -1,6 +1,14 @@
 module Datagraph::Client
   ##
+  # Represents a Datagraph.org user account.
   class Account < Resource
-    # TODO
+    attr_reader :name
+
+    ##
+    # @param  [String, #to_s] name
+    def initialize(name)
+      @name = name.to_s
+      super(Datagraph::URL.join(@name))
+    end
   end # class Account
 end # module Datagraph::Client
