189069de3e84e3570b9564b2ae8fd20c92a6d25f
Arto Bendiken <arto@bendiken.net>
2010-10-19 18:44:18 +0200

    Implemented a Datagraph::Client.xmlrpc method.

--- lib/datagraph/client.rb
+++ lib/datagraph/client.rb
@@ -1,3 +1,5 @@
+require 'xmlrpc/client'
+
 module Datagraph
   ##
   # Datagraph.org API client library.
@@ -6,5 +8,14 @@ module Datagraph
     autoload :Account,    'datagraph/client/account'
     autoload :Repository, 'datagraph/client/repository'
     autoload :Query,      'datagraph/client/query'
+
+    ##
+    # Returns a Datagraph.org XML-RPC API client.
+    #
+    # @return [XMLRPC::Client]
+    # @see    http://ruby-doc.org/stdlib/libdoc/xmlrpc/rdoc/classes/XMLRPC/Client.html
+    def self.xmlrpc
+      XMLRPC::Client.new2(Datagraph::URL.join('xmlrpc'))
+    end
   end
 end
