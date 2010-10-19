9b548b49f1909ea49e4e7da5c1d22d6c3738a35a
Arto Bendiken <arto@bendiken.net>
2010-10-20 01:00:01 +0200

    Implemented the `datagraph list` command.

--- lib/datagraph/command/list.rb
+++ lib/datagraph/command/list.rb
@@ -4,10 +4,13 @@ module Datagraph
     # Lists existing repositories.
     class List < Command
       ##
-      # @param  [Array<String>] repository_specs
+      # @param  [Array<String>] resource_specs
       # @return [void]
-      def execute(*repository_specs)
-        # TODO
+      def execute(*resource_specs)
+        resources = parse_resource_specs(resource_specs) # TODO
+        Repository.each do |repository|
+          puts repository.to_s
+        end
       end
     end # List
   end # Command
