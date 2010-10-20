53d7165db1ad95a062aa73b69b2d294e7151610c
Arto Bendiken <arto@bendiken.net>
2010-10-20 23:38:24 +0200

    Enhanced command-line argument handling in the `datagraph query` command.

--- lib/datagraph/command/query.rb
+++ lib/datagraph/command/query.rb
@@ -7,8 +7,13 @@ module Datagraph
       # @param  [String] repository_spec
       # @param  [String, #to_s] query
       # @return [void]
-      def execute(repository_spec, query)
+      def execute(repository_spec, query = nil)
         repository = validate_repository_specs([repository_spec]).first
+        query = case query
+          when nil       then $stdin.read             # < filename.rq
+          when /^@(.*)$/ then open($1) { |f| f.read } # @filename.rq, @url.rq
+          else query.to_s                             # 'SELECT ...'
+        end
         process = repository.query(query)
         puts "Query #{process} successfully started." if $VERBOSE # FIXME
       end
