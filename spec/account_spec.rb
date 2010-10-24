aa420474ac77e2ad24d15bb75dfbf14661d14fd8
Arto Bendiken <arto@bendiken.net>
2010-10-24 18:57:11 +0200

    Implemented specs for accessing accounts and repositories.

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,5 +1,65 @@
 require File.join(File.dirname(__FILE__), 'spec_helper')
 
 describe Datagraph::Account do
-  # TODO
+  before :all do
+    @account = Datagraph::Account.new('jhacker') # the demo account
+  end
+
+  context "Account#exists?" do
+    it "should not raise an exception" do
+      lambda { @account.exists? }.should_not raise_error
+    end
+
+    it "should return true if the repository exists" do
+      @account.exists?.should == true
+    end
+
+    it "should return false if the repository does not exist" do
+      Datagraph::Account.new('foobar').exists?.should == false
+    end
+  end
+
+  context "Account#name" do
+    it "should not raise an exception" do
+      lambda { @account.name }.should_not raise_error
+    end
+
+    it "should return a string" do
+      @account.name.should be_a(String)
+    end
+
+    it "should return the account name" do
+      @account.name.should == 'jhacker'
+    end
+  end
+
+  context "Account#repository(name)" do
+    it "should require one argument" do
+      lambda { @account.repository() }.should raise_error(ArgumentError)
+      lambda { @account.repository('foaf') }.should_not raise_error(ArgumentError)
+    end
+
+    it "should not raise an exception" do
+      lambda { @account.repository('foaf') }.should_not raise_error
+    end
+
+    it "should return a repository" do
+      @account.repository('foaf').should be_a(Datagraph::Repository)
+    end
+  end
+
+  context "Account#repositories" do
+    it "should not raise an exception" do
+      lambda { @account.repositories }.should_not raise_error
+    end
+
+    it "should return an array of repositories" do
+      @account.repositories.should be_a(Array)
+      @account.repositories.each { |r| r.should be_a(Datagraph::Repository) }
+    end
+
+    it "should return the repositories belonging to the account" do
+      @account.repositories.should == [@account.repository('foaf')]
+    end
+  end
 end
--- spec/repository_spec.rb
+++ spec/repository_spec.rb
@@ -1,5 +1,89 @@
 require File.join(File.dirname(__FILE__), 'spec_helper')
 
 describe Datagraph::Repository do
-  # TODO
+  before :all do
+    @repository = Datagraph::Repository.new('jhacker', 'foaf') # a demo repository
+  end
+
+  context "Repository#exists?" do
+    it "should not raise an exception" do
+      lambda { @repository.exists? }.should_not raise_error
+    end
+
+    it "should return true if the repository exists" do
+      @repository.exists?.should == true
+    end
+
+    it "should return false if the repository does not exist" do
+      Datagraph::Repository.new('jhacker', 'fooabar').exists?.should == false
+    end
+  end
+
+  context "Repository#account" do
+    it "should not raise an exception" do
+      lambda { @repository.account }.should_not raise_error
+    end
+
+    it "should return an account" do
+      @repository.account.should be_a(Datagraph::Account)
+    end
+
+    it "should return the correct account" do
+      @repository.account.should == Datagraph::Account.new('jhacker')
+    end
+  end
+
+  context "Repository#name" do
+    it "should not raise an exception" do
+      lambda { @repository.name }.should_not raise_error
+    end
+
+    it "should return a string" do
+      @repository.name.should be_a(String)
+    end
+
+    it "should return the account name" do
+      @repository.name.should == 'foaf'
+    end
+  end
+
+  context "Repository#summary" do
+    it "should not raise an exception" do
+      lambda { @repository.summary }.should_not raise_error
+    end
+
+    it "should return a string" do
+      @repository.summary.should be_a(String)
+    end
+  end
+
+  context "Repository#description" do
+    it "should not raise an exception" do
+      lambda { @repository.description }.should_not raise_error
+    end
+
+    it "should return a string" do
+      @repository.description.should be_a(String)
+    end
+  end
+
+  context "Repository#created" do
+    it "should not raise an exception" do
+      lambda { @repository.created }.should_not raise_error
+    end
+
+    it "should return a time" do
+      @repository.created.should be_a(XMLRPC::DateTime) # FIXME
+    end
+  end
+
+  context "Repository#updated" do
+    it "should not raise an exception" do
+      lambda { @repository.updated }.should_not raise_error
+    end
+
+    it "should return a time" do
+      @repository.created.should be_a(XMLRPC::DateTime) # FIXME
+    end
+  end
 end
