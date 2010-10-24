f277d59f56d192aebfafec5e72c9d80599a9b664
Arto Bendiken <arto@bendiken.net>
2010-10-24 02:08:57 +0200

    Factored out the SOCKS proxy initialization into a Datagraph::Client.setup! method.

--- lib/datagraph/client.rb
+++ lib/datagraph/client.rb
@@ -16,5 +16,22 @@ module Datagraph
       require 'xmlrpc/client' unless defined?(XMLRPC::Client)
       XMLRPC::Client.new2(Datagraph::URL.join('rpc')) # defaults to XML-RPC for now
     end
+
+    ##
+    # @private
+    # @return [void]
+    def self.setup!(options = {})
+      if (socks_server = ENV['SOCKS_SERVER']) && !socks_server.empty?
+        begin
+          host, port = socks_server.split(':', 2)
+          require 'socksify' # @see http://rubygems.org/gems/socksify
+          TCPSocket::socks_server = host
+          TCPSocket::socks_port   = port.to_i
+          nil
+        rescue LoadError => e
+          warn "WARNING: install the 'socksify' gem to use your configured SOCKS proxy (#{socks_server})."
+        end
+      end
+    end
   end # Client
 end # Datagraph
