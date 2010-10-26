21789d751908bfdd9da9f7cb732ee4d36f39a8c5
Arto Bendiken <arto@bendiken.net>
2010-10-26 23:13:51 +0200

    Implemented `datagraph query` results output in SPARQL JSON format.

--- lib/datagraph/command/query.rb
+++ lib/datagraph/command/query.rb
@@ -32,17 +32,40 @@ module Datagraph
             when Array
               require 'rdf/json'
               parser = RDF::JSON::Reader.new(StringIO.new("{}"))
-              result.each do |bindings|
+              solutions = result.map do |bindings|
                 bindings.each do |k, v|
                   bindings[k] = parser.parse_object(v)
                 end
                 solution = RDF::Query::Solution.new(bindings)
-                $stdout.puts solution.to_hash.inspect # FIXME
               end
+              $stdout.puts to_sparql_json(solutions).to_json
           end
         rescue XMLRPC::FaultException => e # FIXME: JSON-RPC
           $stderr.puts e.message
         end
+      end
+
+      ##
+      # @private
+      def to_sparql_json(solutions)
+        json = {:head => {:vars => []}, :results => {:bindings => []}}
+        solutions.each do |solution|
+          solution.each_name do |k|
+            json[:head][:vars] << k unless json[:head][:vars].include?(k)
+          end
+          json[:results][:bindings] << solution.to_hash.each.inject({}) do |bindings, (k, v)|
+            bindings.merge(k => case v
+              when RDF::Node     then {:type => :bnode, :value => v.id.to_s}
+              when RDF::URI      then {:type => :uri, :value => v.to_s}
+              when RDF::Literal  then case
+                when v.datatype? then {:type => :literal, :value => v.to_s, :datatype => v.datatype.to_s}
+                when v.language? then {:type => :literal, :value => v.to_s, 'xml:lang' => v.language.to_s}
+                else {:type => :literal, :value => v.to_s}
+              end
+            end)
+          end
+        end
+        json
       end
     end # Query
   end # Command
