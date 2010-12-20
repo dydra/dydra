53bb7342d2212a3a7def32fc20970e4a242b9644
Arto Bendiken <arto@bendiken.net>
2010-12-20 10:52:45 +0100

    Cleaned up the XML-RPC client UTF-8 patch a little bit.

---
+++ lib/datagraph/patches/xmlrpc.rb
@@ -0,0 +1,15 @@
+##
+# Patches for Ruby's built-in XML-RPC client.
+class XMLRPC::Client
+  alias_method :do_rpc_broken, :do_rpc
+
+  ##
+  # This patch is a workaround for a REXML bug in Ruby 1.9.2.
+  #
+  # @see http://www.ruby-forum.com/topic/463233
+  def do_rpc(request, async = false)
+    data = do_rpc_broken(request, async)
+    data.force_encoding(Encoding::UTF_8) if data.respond_to?(:force_encoding)
+    data
+  end
+end # XMLRPC::Client
