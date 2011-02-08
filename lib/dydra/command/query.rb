5d1788ffe34376872bb2a878b676071f50083b45
Ben Lavender <blavender@gmail.com>
2011-02-09 00:23:15 +0100

    Implement error handling for the query command

--- lib/dydra/command/query.rb
+++ lib/dydra/command/query.rb
@@ -8,27 +8,35 @@ module Dydra
       # @param  [String, #to_s] query
       # @return [void]
       def execute(repository_spec, query)
-        repository = Repository.new(repository_spec)
-        @query = case query
-          when nil       then $stdin.read             # < filename.rq
-          when /^@(.*)$/ then open($1) { |f| f.read } # @filename.rq, @url.rq
-          else query.to_s                             # 'SELECT ...'
-        end
-        @options[:result_format] ||= :parsed
-        format = @options[:result_format] == :xml ? :xml : :json
-        result = repository.query(@query, @options[:result_format])
-        if @options[:result_format] == :parsed
-          if result.respond_to?(:empty?) && !result.empty?
-            variables = result.first.each_name.to_a
-            puts variables.join("\t")
-            result.each do |binding|
-              puts variables.map { |n| binding.to_hash[n] }.join("\t")
+        begin
+          repository = Repository.new(repository_spec)
+          @query = case query
+            when nil       then $stdin.read             # < filename.rq
+            when /^@(.*)$/ then open($1) { |f| f.read } # @filename.rq, @url.rq
+            else query.to_s                             # 'SELECT ...'
+          end
+          @options[:result_format] ||= :parsed
+          format = @options[:result_format] == :xml ? :xml : :json
+          result = repository.query(@query, @options[:result_format])
+          if @options[:result_format] == :parsed
+            if result.respond_to?(:empty?) && !result.empty?
+              variables = result.first.each_name.to_a
+              puts variables.join("\t")
+              result.each do |binding|
+                puts variables.map { |n| binding.to_hash[n] }.join("\t")
+              end
+            else
+              puts result
             end
           else
             puts result
           end
-        else
-          puts result
+        rescue RepositoryMisspecified => e
+          puts e
+        rescue RestClient::Forbidden
+          puts "Insufficient permissions to query #{repository_spec}"
+        rescue RestClient::ResourceNotFound
+          puts "#{repository_spec} not found"
         end
       end
 
