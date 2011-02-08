f6960d0c7450e1df05599b8309ede4d8a0610ce9
Ben Lavender <blavender@gmail.com>
2011-02-09 00:55:37 +0100

    Improve login error handling

--- lib/dydra/command/login.rb
+++ lib/dydra/command/login.rb
@@ -10,20 +10,24 @@ module Dydra
       # @return [void]
       def execute(user = nil, pass = nil)
         while !Dydra::Client.setup?
+          Dydra::Client.reset!
+          user = pass = nil
           begin
             user = ask_for_user if user.nil?
             pass = ask_for_pass if pass.nil?
             Dydra::Client.setup!(:user => user, :pass => pass)
             token = Account.new(user).info['authentication_token']
             raise AuthenticationError, "Incorrect password" if token.nil?
+          rescue RestClient::ResourceNotFound
+            puts "User #{user} not found"
+          rescue SignalException, SystemExit => e
+            # Special case ctrl-c at the command line
+            puts "caught #{e}"
+            raise e 
           rescue Exception => e
-            # Special case ctrl-c
-            raise e if (e.is_a?(SignalException) || e.is_a?(SystemExit) )
             puts "Invalid credentials: #{e.message}"
-            Dydra::Client.reset!
-            user = pass = nil
-            next
           end
+          Dydra::Client.reset! unless token
         end
         save_credentials(user, token)
         puts "Credentials saved to ~/.dydra/credentials" if verbose?
