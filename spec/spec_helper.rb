017612b1595b881e804ee4c5710c3d139955db2e
Arto Bendiken <arto@bendiken.net>
2011-02-09 04:21:56 +0100

    Ensured that the RSpec helper calls Dydra.setup! so that specs can run.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,8 +2,10 @@ require 'dydra'
 require 'rdf/spec'
 
 RSpec.configure do |config|
-  config.include(RDF::Spec::Matchers)
+  config.include RDF::Spec::Matchers
   config.exclusion_filter = {:ruby => lambda { |version|
     RUBY_VERSION.to_s !~ /^#{version}/
   }}
 end
+
+Dydra.setup!
