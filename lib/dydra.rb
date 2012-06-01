5fe360eb3e1c8db50b8adc6788a2ea4a998e5576
Arto Bendiken <arto@bendiken.net>
2012-06-01 14:33:12 +0200

    Ensured that Dydra.authenticate! loads the ~/.dydra/environment file.

--- lib/dydra.rb
+++ lib/dydra.rb
@@ -43,18 +43,88 @@ module Dydra
   class QueryError < ::StandardError; end
 
   ##
-  # @private
-  # @see    Client.authenticate!
+  # Authenticates with Dydra.com.
+  #
+  # @param  [Hash{Symbol => Object}] options
+  # @option options [String] :token    (nil)
+  # @option options [String] :account  (nil)
+  # @option options [String] :password (nil)
   # @return [void]
-  def self.authenticate!(*args)
-    Client.authenticate!(*args)
+  def self.authenticate!(options = {})
+    self.setup!(options)
   end
 
   ##
   # @private
-  # @see    Client.setup!
+  # @param  [Hash{Symbol => Object}] options
+  # @option options [String] :token    (nil)
+  # @option options [String] :account  (nil)
+  # @option options [String] :password (nil)
   # @return [void]
-  def self.setup!(*args)
-    Client.setup!(*args)
+  def self.setup!(options = {})
+    unless options.empty?
+      case
+        when options[:token]
+          ENV['DYDRA_TOKEN'] = options[:token].to_s
+        when options[:account]
+          ENV['DYDRA_ACCOUNT'] = options[:account].to_s
+          ENV['DYDRA_PASSWORD'] = options[:password].to_s
+        when options[:user] # @deprecated
+          ENV['DYDRA_ACCOUNT'] = options[:user].to_s
+          ENV['DYDRA_PASSWORD'] = (options[:password] || options[:pass]).to_s
+      end
+    else
+      self.load_environment! or self.load_credentials!
+    end
+    self.setup_socks_proxy!
+  end
+
+  ##
+  # @private
+  def self.load_environment!
+    filepath = File.join(ENV['HOME'], '.dydra', 'environment')
+    return false unless File.exists?(filepath)
+
+    File.open(filepath, 'r') do |f|
+      f.each_line do |line|
+        case line
+          when /^\s*#/ then next # skip comments
+          when /^([A-Za-z_][A-Za-z0-9_]+)\s*=\s*(\S+)/
+            ENV[$1] = $2 unless ENV.has_key?($1)
+          else # TODO: error handling
+        end
+      end
+    end
+    true
+  end
+
+  ##
+  # @deprecated
+  # @private
+  def self.load_credentials!
+    filepath = File.join(ENV['HOME'], '.dydra', 'credentials')
+    return false unless File.exists?(filepath)
+
+    require 'yaml'
+    yaml = YAML.load_file(filepath)
+    ENV['DYDRA_ACCOUNT'] = yaml[:user] if yaml.has_key?(:user)
+    ENV['DYDRA_PASSWORD'] = yaml[:password] if yaml.has_key?(:password)
+    ENV['DYDRA_TOKEN'] = yaml[:token] if yaml.has_key?(:token)
+    true
+  end
+
+  ##
+  # @private
+  def self.setup_socks_proxy!
+    if (socks_server = ENV['SOCKS_SERVER']) && !socks_server.empty?
+      begin
+        host, port = socks_server.split(':', 2)
+        require 'socksify' # @see http://rubygems.org/gems/socksify
+        TCPSocket::socks_server = host
+        TCPSocket::socks_port   = port.to_i
+      rescue LoadError => e
+        warn "WARNING: install the 'socksify' gem to use your configured SOCKS proxy (#{socks_server})." if $VERBOSE
+      end
+    end
   end
 end # Dydra
--- lib/dydra/client.rb
+++ lib/dydra/client.rb
@@ -5,11 +5,6 @@ require 'rest_client'   # @see http://rubygems.org/gems/rest-client
 require 'json'          # @see http://rubygems.org/gems/json
 
 module Dydra
-  ##
-  # Implements a Dydra.com XML-RPC API client.
-  #
-  # @see http://docs.dydra.com/sdk/ruby
-  # @see http://docs.dydra.com/api/rpc
   module Client
     ##
     # Returns a Dydra.com RPC API client.
@@ -26,34 +21,6 @@ module Dydra
         XMLRPC::Client.new3('host' => url, 'port' => port, 'path' => '/rpc', 'user' => ENV['DYDRA_ACCOUNT'], 'password' => ENV['DYDRA_PASSWORD'])
       else
         raise AuthenticationError, "You need to run Dydra.setup! before performing an API operation"
-      end
-    end
-
-    ##
-    # Authenticates with Dydra.com.
-    #
-    # @param  [Hash{Symbol => Object}] options
-    # @option options [String] :token    (nil)
-    # @option options [String] :user     (nil)
-    # @option options [String] :password (nil)
-    # @return [void]
-    def self.authenticate!(options = {})
-      if options[:token]
-        ENV['DYDRA_TOKEN'] = options[:token]
-      elsif options[:user]
-        ENV['DYDRA_ACCOUNT'] = options[:user]
-        ENV['DYDRA_PASSWORD'] = options[:password] || options[:pass] || raise(AuthenticationError, "You must supply a password")
-      elsif File.exists?(credentials_file)
-        require 'yaml'
-        yaml = YAML.load_file(credentials_file)
-        ENV['DYDRA_ACCOUNT'] = yaml[:user] if yaml.has_key?(:user)
-        ENV['DYDRA_PASSWORD'] = yaml[:password] if yaml.has_key?(:password)
-        ENV['DYDRA_TOKEN'] = yaml[:token] if yaml.has_key?(:token)
-        if !ENV['DYDRA_ACCOUNT'] && !(ENV['DYDRA_PASSWORD'] || ENV['DYDRA_TOKEN'])
-          raise AuthenticationError, "You need to specify :user and :pass or :token in #{credentials_file}"
-        end
-      else
-        raise AuthenticationError, "You need to give either :user and :password, or :token, or set your ENV['DYDRA_TOKEN'] to authenticate."
       end
     end
 
@@ -116,49 +83,6 @@ module Dydra
       else
         raise AuthenticationError, "You need to run Dydra.setup! before performing an API operation"
       end
-    end
-
-    ##
-    # @private
-    # @return [void]
-    def self.setup!(options = {})
-      authenticate!(options)
-      if (socks_server = ENV['SOCKS_SERVER']) && !socks_server.empty?
-        begin
-          host, port = socks_server.split(':', 2)
-          require 'socksify' # @see http://rubygems.org/gems/socksify
-          TCPSocket::socks_server = host
-          TCPSocket::socks_port   = port.to_i
-          nil
-        rescue LoadError => e
-          warn "WARNING: install the 'socksify' gem to use your configured SOCKS proxy (#{socks_server})." if $VERBOSE
-        end
-      end
-    end
-
-    ##
-    # Returns `true` if `#setup!` has been run.
-    #
-    # @return [Boolean] `true` or `false`
-    def self.setup?
-      true # FIXME
-    end
-
-    ##
-    # Clears the current user state.
-    #
-    # @return [void]
-    def self.reset!
-      # TODO
-    end
-
-    ##
-    # Returns the path of the file which we'll read and write credentials
-    # to.
-    #
-    # @return [String]
-    def self.credentials_file
-      File.join(ENV['HOME'], '.dydra', 'credentials') # FIXME
     end
   end # Client
 end # Dydra
